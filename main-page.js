dbb892bc36efe166e450df403a9cd62016df4e1a
casino-wheels-and-gears <freescrap226@gmail.com>
2024-11-29 12:22:15 +0200

    Add files via upload

---
+++ main-page.js
@@ -0,0 +1,21 @@
+const wheel = document.getElementById("wheel")
+const winButton = document.getElementById("win-button")
+const winPopup = document.getElementById("ad-popup1")
+
+let currentRotation = 0
+winButton.onclick = () => 
+{
+    let rotateDegrees = Math.random()*360*3+360*3
+    currentRotation += rotateDegrees
+    let transDuration = currentRotation/360
+    wheel.style.transitionDuration = transDuration+"s"
+    wheel.style.transform = "rotate("+currentRotation+"deg)"
+    setTimeout(() => {
+        let popupDuration = Math.random() * 6 + 1
+        winPopup.style.animation = `ad-popup-anim ${popupDuration}s`
+        // winPopup.style.animationDuration = transDuration + "s"
+        setTimeout(() => {
+            winPopup.style.animation = ""
+        }, 5000)
+    }, transDuration*1000)
+}
